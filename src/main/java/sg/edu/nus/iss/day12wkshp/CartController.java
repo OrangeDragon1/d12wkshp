a414ce9f22bfd3ffa3acc6b79672a405cad3498a
OrangeDragon1 <wcks095@gmail.com>
2022-07-19 13:16:08 +0800

    small adjustments

--- src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import sg.edu.nus.iss.day12wkshp.CartService;
-
 @Controller
 @RequestMapping(path = { "/cart" })
 public class CartController {
